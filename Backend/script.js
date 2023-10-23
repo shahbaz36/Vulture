4fcb75f7b86b9b75df246edf815e8ca8ebc24e8f
Shahbaz Ali <aquibahmad345@gmail.com>
2023-10-23 17:24:09 +0530

    connect to database

---
+++ Backend/script.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB connection successful!'));
+
+app.listen(port,()=>{
+    console.log(`App running on port ${port}...`);
+})
--- script.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".typing", {
-    strings: ["Defend, Detect, and Stay Secure Online"],
-    typeSpeed: 140,
-    // backSpeed: 160,
-    // loop: true
-})
