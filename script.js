9c2e0e4dedb4a210fe218d7421290b2b076bd1e4
kaif-imteyaz <kaifimteyaz.k@gmail.com>
2023-10-28 06:27:35 +0530

    frontend

---
+++ script.js
@@ -0,0 +1,7 @@
+ // typing text animation script
+ var typed = new Typed(".typing", {
+    strings: ["Defend, Detect, and Stay Secure Online"],
+    typeSpeed: 150,
+    // backSpeed: 60,
+    loop: true
+});
